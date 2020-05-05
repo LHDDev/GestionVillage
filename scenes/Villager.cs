cc7d64b1b985ef4e6d64fbcf492ac151b82628e0
La Hyène Déphasée <anthony.zadikian@gmail.com>
2020-05-05 08:44:02 +0200

    Création de la branche Interactions_Souris permettant d'intégrer toutes les interactions entre la souris et le jeu

--- scenes/Villager.cs
+++ scenes/Villager.cs
@@ -16,6 +16,7 @@ public class Villager : KinematicBody2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		worldBoundaries = new Vector2(500, 500);
 		cooldown = new Timer();
 		AddChild(cooldown);
 		cooldown.Connect("timeout", this, "move");
@@ -34,27 +35,47 @@ public class Villager : KinematicBody2D
 		Random rndDirection = new Random();
 		bool canMove = false;
 
-		switch ((DIRECTIONS)rndDirection.Next(5))
+		//switch ((DIRECTIONS)rndDirection.Next(5))
+		switch((DIRECTIONS)1)
 		{
 			case DIRECTIONS.NORTH:
-				this.dir.x = 0;
-				this.dir.y = -speed;
+				dir.x = 0;
+				dir.y = -speed;
 				break;
 			case DIRECTIONS.EAST:
-				this.dir.x = speed;
-				this.dir.y = 0;
+				dir.x = speed;
+				dir.y = 0;
 				break;
 			case DIRECTIONS.WEST:
-				this.dir.x = -speed;
-				this.dir.y = 0;
+				dir.x = -speed;
+				dir.y = 0;
 				break;
 			case DIRECTIONS.SOUTH:
-				this.dir.x = 0;
-				this.dir.y = speed;
+				dir.x = 0;
+				dir.y = speed;
 				break;
 		}
 		cooldown.WaitTime = 1;
 		cooldown.Start();
 
+		canMove = canIMove(this.Position + this.dir);
+
+		if(!canMove)
+		{
+			this.dir = Vector2.Zero;
+		}
+	}
+
+	private bool canIMove(Vector2 newPosition)
+	{
+		bool result = true;
+
+		if (newPosition.x <= 0 || newPosition.x >= worldBoundaries.x)
+			result = false;
+		if (newPosition.y <= 0 || newPosition.y >= worldBoundaries.x)
+			result = false;
+
+		GD.Print("Can I move ? " + newPosition + " => " +result);
+		return result;
 	}
 }
