f74b136bb9bc13941129d69486e0131b16062f2c
La Hyène Déphasée <anthony.zadikian@gmail.com>
2020-05-05 14:10:38 +0200

    Ajout de la fonction canIMove(new_pos) qui va permettre de vérifier si le joueur peut se déplacer à cette position ou non. La prochaine étape est de vérifier si la nouvellle position n'est pas déjà occupée. Dans World.gd : ajout de la fonction can_spawn(pos) qui va vérifier si un villageois peut spawn à la dite position. Close #1

--- scripts/Villager.gd
+++ scripts/Villager.gd
@@ -27,6 +27,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
 	move_and_collide(dir*delta)
 	
 func move():
--- scripts/World.gd
+++ scripts/World.gd
@@ -13,6 +13,9 @@ func _ready():
 		var pos = Vector2()
 		pos.x = rand_range(100,chunkSize)
 		pos.y = rand_range(100,chunkSize)
+		while not canSpawn(pos) :
+			pos.x = rand_range(100,chunkSize)
+			pos.y = rand_range(100,chunkSize)
 		newVillager.position = pos
 		add_child(newVillager)
 		
@@ -21,3 +24,13 @@ func _draw():
 	var chunk = Vector2(chunkSize,chunkSize)
 	var spawn_zone = Rect2(-chunk / 2 ,chunk)
 	draw_rect(spawn_zone,Color.red,false)
+
+func canSpawn(pos_spawn):
+	# Check si à cette position, il existe pas déjà un élément.
+	# Si il existe : retourner False
+	# Si c'est ok : retourner True
+	for n in get_tree().get_nodes_in_group("villagers"):
+		if (n.position.x - 5 <= pos_spawn.x && n.position.y-5 <= pos_spawn.x) && (n.position.x + 5 >= pos_spawn.x && n.position.y+5 <= pos_spawn.y) :
+			return false
+	return true
+	pass
